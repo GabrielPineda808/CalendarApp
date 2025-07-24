bd7ec57a81469b7c9b614245c0ad96a86c761069
Gabe Pineda <gabepineda6@gmail.com>
2025-07-24 11:06:51 -0700

    first commit

---
+++ src/main/java/com/cal/CalApp/CalAppApplication.java
@@ -0,0 +1,13 @@
+package com.cal.CalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalAppApplication.class, args);
+	}
+
+}
